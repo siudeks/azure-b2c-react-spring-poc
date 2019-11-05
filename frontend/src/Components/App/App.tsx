edbeb7fbe8e6ddde0b54afbb823423f0869d5a5a
Jarosław Gosławski <jaroslaw.goslawski@gft.com>
2019-11-05 17:30:40 +0100

    secure routing

--- frontend/src/Components/App/App.tsx
+++ frontend/src/Components/App/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
+import React,{ useState }   from 'react';
 import './App.css';
 import Login from '../Login/Login';
 import Dashboard from '../Dashboard/Dashboard';
-import { BrowserRouter, Route } from 'react-router-dom';
-import AuthenticationServiceStub from '../../Services/AuthenticationServiceStub';
-import AuthenticationService from '../../Services/Interfaces/AuthenticateService';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import RequireAuthenticationRoute from '../RequireAuthenticationRoute/RequireAuthenticationRoute';
 
 const App: React.FC = () => {
 
-  let authService: AuthenticationService = new AuthenticationServiceStub();
+  const [isAuthenticate,setIsAuthenticate] = useState<Boolean>(false);
 
   return (
     <BrowserRouter>
-      <Route exact path="/">
-        <Login authenticationService={authService} />
-      </Route>
-      <Route exact path="/login">
-        <Login authenticationService={authService} />
-      </Route>
-      <Route path="/dashbaord" component={Dashboard} />
+      <Switch>
+        <Route exact path="/">
+          <Login onAuthenticate={setIsAuthenticate} />
+        </Route>
+        <Route exact path="/login">
+          <Login onAuthenticate={setIsAuthenticate} />
+        </Route>
+        <RequireAuthenticationRoute isAuthenticate={isAuthenticate} redirect="/login" path="/dashbaord">
+          <Dashboard />
+        </RequireAuthenticationRoute>
+      </Switch>
     </BrowserRouter>
   );
 }
---
+++ frontend/src/Components/RequireAuthenticationRoute/RequireAuthenticationRoute.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Route, Redirect, RouteProps } from 'react-router-dom';
+
+
+export interface RequireAuthenticationRouteProps extends RouteProps {
+    isAuthenticate: Boolean,
+    redirect: string
+}
+
+const  RequireAuthenticationRoute: React.FC<RequireAuthenticationRouteProps> = (props: RequireAuthenticationRouteProps) => {
+
+    return (
+        <Route {...props}>
+            {props.children}
+            {props.isAuthenticate ? (props.children) : (<Redirect to={{ pathname: props.redirect }} />)}
+        </Route>
+    );
+}
+
+export default RequireAuthenticationRoute;
