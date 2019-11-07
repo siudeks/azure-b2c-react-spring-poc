cd42cdb511cff585449abe334432ec65a4019d89
jaroslawgoslawski <jaroslaw.goslawski@gft.com>
2019-11-07 10:55:56 +0100

    Code refactor for login functionality

--- frontend/src/Components/Login/Login.tsx
+++ frontend/src/Components/Login/Login.tsx
@@ -2,21 +2,40 @@ import React from 'react'
 import './Login.css'
 import { Redirect, Route, RouteComponentProps, withRouter } from "react-router-dom";
 
-interface  LoginProps extends RouteComponentProps {
+interface LoginProps extends RouteComponentProps {
     onAuthenticate: Function,
     redirect: string,
 }
 
-
 const Login: React.FC<LoginProps> = (props: LoginProps) => {
 
-    if (props.location.hash) {
-        props.onAuthenticate(props.location.hash.split("=")[1]);//Ugly...
+    const policy:       string = process.env.REACT_APP_B2C_POLICY;
+    const clientId:     string = process.env.REACT_APP_B2C_CLIENTID;
+    const scope:        string = process.env.REACT_APP_B2C_SCOPE;
+    const responseType: string = process.env.REACT_APP_B2C_RESPONSE_TYPE;
+    const tenantName:   string = process.env.REACT_APP_B2C_TENANT_NAME;
+    const host:         string = process.env.REACT_APP_B2C_REDIRECT_HOST;
+    const port:         string = process.env.REACT_APP_B2C_REDIRECT_PORT;
+
+    const url: string = `https://${tenantName}.b2clogin.com/${tenantName}.onmicrosoft.com/oauth2/v2.0/authorize?` +
+        `p=${policy}&` +
+        `client_id=${clientId}&` +
+        `nonce=defaultNonce&` +
+        `redirect_uri=http%3A%2F%2F${host}%3A${port}%2F&` +
+        `scope=${scope}&` +
+        `response_type=${responseType}&` +
+        `prompt=login`;
+
+
+    const params: string[] = props.location.hash.split("=");
+
+    if (params.length === 2 && params[0] === `#${responseType}`) {
+        props.onAuthenticate(params[1]);
         return (<Redirect to={{ pathname: props.redirect }} />);
     }
 
-    const url: string = 'https://mastergos.b2clogin.com/mastergos.onmicrosoft.com/oauth2/v2.0/authorize?p=B2C_1_login_poc&client_id=050fb5cc-57e2-424c-9676-8d6062891d9c&nonce=defaultNonce&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2F&scope=openid&response_type=id_token&prompt=login';
     window.location.replace(url);
+
     return (
         <div>
             You will be redirected to the <a href={url}>login page</a>.
