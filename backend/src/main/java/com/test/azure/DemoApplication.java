4db339589541817bd4ffa5f36f107f62ef31c114
Kamil Banaszczyk <kjbanaszczyk@gmail.com>
2019-11-07 12:17:40 +0100

    gitignore

--- backend/src/main/java/com/test/azure/DemoApplication.java
+++ backend/src/main/java/com/test/azure/DemoApplication.java
@@ -2,6 +2,7 @@ package com.test.azure;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
 public class DemoApplication {
