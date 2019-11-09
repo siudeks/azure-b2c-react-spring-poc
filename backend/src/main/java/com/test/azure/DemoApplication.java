1b33195fbe609cba6c30d8009e80cb0a75196a4c
Kamil Banaszczyk <kjbanaszczyk@gmail.com>
2019-11-09 01:28:02 +0100

    Nice java 11 feature showcase

--- backend/src/main/java/com/test/azure/DemoApplication.java
+++ backend/src/main/java/com/test/azure/DemoApplication.java
@@ -4,14 +4,13 @@ import com.test.azure.ad.azureadtest.GreetingWebClient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import lombok.val;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		val gwc = new GreetingWebClient();
+		final var gwc = new GreetingWebClient();
 		System.out.println(gwc.getResult());
 	}
 
