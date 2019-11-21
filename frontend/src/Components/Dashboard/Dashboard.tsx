b2f739c331ed00df94b98d1b882fa57ff1d4ed11
Jarosław Gosławski <jaroslaw.goslawski@gft.com>
2019-11-21 16:47:07 +0100

    Dashboard functionality  (book list and details)

--- frontend/src/Components/Dashboard/Dashboard.tsx
+++ frontend/src/Components/Dashboard/Dashboard.tsx
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { useAllBooksQuery } from '../.generated/components';
+import { useAllBooksQuery, Book } from '../.generated/components';
 import QueryResultDisplayer from '../QueryResultDisplayer/QueryResultDisplayer';
-import BooksList from './BooksList';
+import BooksList from './BooksList/BooksList';
+import BookDetails from './BookDetails/BookDetails';
+import './Dashbaord.sass';
+
 
 const Dashboard: React.FC = () => {
 
     const { data, loading, error } = useAllBooksQuery();
+    const [selectedBook, setSelectedBook] = useState<Book>()
+    let books: Book[] = (data && data.books && data.books.map(b => b as Book)) || [];
 
     return (
         <div>
-            <QueryResultDisplayer loading={loading} error={error}>
-                {data ? (<BooksList {...data} />) : (<p>No data</p>)}
-            </QueryResultDisplayer>
-            <Link to="/logout">Logout</Link>
+            <div>
+                <Link to="/logout">Logout</Link>
+            </div>
+            <div className="component-container">
+                <div className="list-container">
+                    <QueryResultDisplayer loading={loading} error={error}>
+                        <h3> Books: </h3>
+                        <div className="scrollable-container">
+                            <BooksList books={books} onBookSelected={setSelectedBook} />
+                        </div>
+                    </QueryResultDisplayer>
+                </div>
+                {selectedBook ? (<BookDetails {...selectedBook} />) : <div />}
+            </div>
         </div>
     );
 }
