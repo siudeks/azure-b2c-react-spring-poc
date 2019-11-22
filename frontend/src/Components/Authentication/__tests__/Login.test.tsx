ee5617f197f90c4ca4d1875fc9fd9f157a0acb97
jaroslawgoslawski <jaroslaw.goslawski@gft.com>
2019-11-22 23:38:19 +0100

    More login tests

--- frontend/src/Components/Authentication/__tests__/Login.test.tsx
+++ frontend/src/Components/Authentication/__tests__/Login.test.tsx
@@ -1,10 +1,14 @@
 import Authenticator from '../../../Helpers/Authenticator';
+import { authResponseCallback } from "msal/lib-commonjs/UserAgentApplication";
 import Login, { LoginProps } from '../Login'
 import { shallow } from 'enzyme';
 import React from 'react';
+import { Account, AuthError } from 'msal';
 
 
 describe('Login component', () => {
+
+    const helloWorldText = "Hello World!";
 
 
     it('should automatically redirect to login page when rendered', () => {
@@ -13,13 +17,82 @@ describe('Login component', () => {
         const props = createLoginPropsMock();
 
         //Act
-        shallow(<Login {...props} ></Login>);
+        shallowRenderLoginComponent(props);
 
         //Assert
         expect(props.authenticator.loginRedirect).toBeCalledTimes(1);
     });
 
+    it('should display information about redirecting', () => {
+
+        //Arrange
+        const props = createLoginPropsMock();
+
+        //Act
+        const underTest = shallowRenderLoginComponent(props);
+
+        //Assert
+        expect(underTest.text()).toEqual("You will be redirected to the login page.");
+    });
+
+    it('should NOT display children component when NOT authenticated', () => {
+
+        //Arrange
+        const props = createLoginPropsMock();
+
+        //Act
+        const underTest = shallowRenderLoginComponent(props);
+
+        //Assert
+        expect(underTest.text()).not.toEqual(helloWorldText);
+    });
+
+    it('should display children component when authenticated', () => {
+
+        //Arrange
+        const props = createLoginPropsMock();
+        const getAccountMock = jest.fn().mockReturnValue(createEmptyAccount());
+        const propsWithGetAccountMock = { authenticator: { ...props.authenticator, getAccount: getAccountMock } };
+
+        //Act
+        const underTest = shallowRenderLoginComponent(propsWithGetAccountMock);
+
+        //Assert
+        expect(underTest.text()).toEqual(helloWorldText);
+    });
+
+
+    it('should display information about authentication error when occurred', () => {
+
+        //Arrange
+        const props = createLoginPropsMock();
+
+        let wasCalled = false;
+        const callbackFn = (authCallback: authResponseCallback) => {
+            if(wasCalled) return;
+            wasCalled = true;
+            authCallback(new AuthError("1"));
+        }
   
+        const handleRedirectCallbackMock = jest.fn(callbackFn);
+        const propsWitHhandleRedirectCallbacktMock = { authenticator: { ...props.authenticator, handleRedirectCallback: handleRedirectCallbackMock } };
+
+        //Act
+        const underTest = shallowRenderLoginComponent(propsWitHhandleRedirectCallbacktMock);
+
+        //Assert
+        expect(underTest.text()).toEqual("Authentication error occurred.");
+    });
+
+    //Helper
+    const shallowRenderLoginComponent = (props: LoginProps) => {
+        return shallow(
+            <Login {...props} >
+                <div>{helloWorldText}</div>
+            </Login>
+        );
+    }
+
     const createLoginPropsMock = (): LoginProps => {
         const authenticatorMock: Authenticator = {
             logout: jest.fn(),
@@ -33,6 +106,8 @@ describe('Login component', () => {
             authenticator: authenticatorMock
         };
     }
+
+    const createEmptyAccount = (): Account => new Account("", "", "", "", {}, "", "");
 
 });
 
